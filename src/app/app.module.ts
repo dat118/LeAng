ad27098c2edd0aa340a82fa3e3a29dc5bbd0458c
dat118 <dat1182000@gmail.com>
2023-07-19 08:47:29 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ListComponent } from './recipe/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SidebarComponent,
+    RecipeComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
